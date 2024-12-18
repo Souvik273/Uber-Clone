d65effebd47981f9b1408368f024287964d1bea0
Souvik273 <souvikgoswami464@gmail.com>
2024-12-19 00:46:24 +0530

    Backend Updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 const { initializeSocket } = require('./socket');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5178;
 
 const server = http.createServer(app);
 
@@ -9,4 +9,4 @@ initializeSocket(server);
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-});
+}); 
